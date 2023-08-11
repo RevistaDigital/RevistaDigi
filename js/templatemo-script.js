f7523df8c8cdde70305ba68b88f70e1408cc5121
RevistaDigital <136780229+RevistaDigital@users.noreply.github.com>
2023-08-11 14:50:00 -0300

    Add files via upload

---
+++ js/templatemo-script.js
@@ -0,0 +1,25 @@
+$(function() {
+    $(".navbar-toggler").on("click", function(e) {
+        $(".tm-header").toggleClass("show");
+        e.stopPropagation();
+      });
+    
+      $("html").click(function(e) {
+        var header = document.getElementById("tm-header");
+    
+        if (!header.contains(e.target)) {
+          $(".tm-header").removeClass("show");
+        }
+      });
+    
+      $("#tm-nav .nav-link").click(function(e) {
+        $(".tm-header").removeClass("show");
+      });
+});
+
+$(document).ready(function () {
+  $('.tm-nav-link').click(function (e) {
+      e.preventDefault();
+      $(this).parent().toggleClass('active');
+  });
+});
